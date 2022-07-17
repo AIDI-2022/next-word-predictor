afd03ddb9e64d7b07f6ea42221b57eecd6c6f235
Sharath Kumar <tcsharath6497@gmail.com>
2022-07-17 02:32:27 -0400

    added models in consumers. Serializer - to be developed

--- apps/nwp/consumers.py
+++ apps/nwp/consumers.py
@@ -2,7 +2,7 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from .utils import predict_next_word
 import json, datetime
-
+from .views import nwp_socket
 
 class NwpConsumer(WebsocketConsumer):
     def connect(self):
@@ -17,8 +17,10 @@ class NwpConsumer(WebsocketConsumer):
     def receive(self,text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json.get('message')
-        print(message)
-        predicted_words = predict_next_word(message)
+        resp = nwp_socket(message)
+        predicted_words = resp.get('data')
+        print('response----------',resp)
+        # predicted_words = predict_next_word(message)
 
         self.send(text_data=json.dumps({
             'type':'chat',
