35053e1f6db07e63f5f9105cf17116d1907bde0d
Navaneeth J <Navaneethjawahar333@gmail.com>
2022-08-09 15:55:02 -0400

    endpoint for azure

--- core/settings.py
+++ core/settings.py
@@ -27,8 +27,8 @@ DEBUG = True #env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1') ]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
+ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1',               env('SERVER', default='127.0.0.1'), '.azure.com' ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ,'.azure.com']
 
 # Application definition
 
